fd7abfd089781ce0ac7799e7c97404988a69b58e
Andrew <andrew.j.hui@gmail.com>
2019-08-19 23:20:28 +0000

    AssertionEqual update with $ and head file

--- lotide/head.js
+++ lotide/head.js
@@ -7,6 +7,18 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(theArray) {
+  if (theArray === []) {
+    return undefined;
+  }
+  else {
+    return theArray[0];
+  }
+}
+
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
