e7da093a8b317787e454fd47814e79b4382591d7
Andrew <andrew.j.hui@gmail.com>
2019-08-26 21:00:31 +0000

    remove gitignore files

--- lotide/assertArraysEqual.js
+++
@@ -1,8 +0,0 @@
-const assertEqual = require('./assertEqual');
-const eqArrays = require('./eqArrays')
-
-const assertArraysEqual = function (firArray, secArray, trueOrFalse) {
-  return assertEqual(eqArrays(firArray, secArray), trueOrFalse);
-}
-
-module.exports = assertArraysEqual;
--- lotide/test/middleTest.js
+++
@@ -1,7 +0,0 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
-
-// TEST CODE
-assertArraysEqual(middle([1, 2, 3]), [2], true);
-assertArraysEqual(middle([1, 2]), [1], false);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3], true);
